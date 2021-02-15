edce1a57ae05f6cdd64317c806308d638bb69cd8
bigbae18 <niniohhns@gmail.com>
2021-02-15 16:13:45 +0100

    minor fixes

--- routes/User.js
+++ routes/User.js
@@ -6,7 +6,6 @@ const saltRounds = 10;
 const db = require('../config/db');
 
 router.post('/register', async (req, res) => {
-    console.log(req.bodyz);
     const username = req.body.username;
     const hash = await bcrypt.hash(req.body.password, saltRounds);
     const email = req.body.email;
@@ -17,15 +16,13 @@ router.post('/register', async (req, res) => {
     db.query(checkUsernameQuery, [username, email], (error, result) => {
         if (error) {
             console.log(error);
-            res.json({
+            res.status(500).json({
                 message: "There were an error with database connection",
-                error: error,
-                code: 500
+                error: error
             });
         } else if (result.length > 0) {
-            res.json({
-                message: "Username/e-mail already exists",
-                code: 401
+            res.status(401).json({
+                message: "Username/e-mail already exists"
             })
         } else {
             const registerQuery = "INSERT INTO `users` (username, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?);";
@@ -34,16 +31,13 @@ router.post('/register', async (req, res) => {
             db.query(registerQuery, [username, hash, email, time, time], (error, result) => {
                 if (error) {
                     console.log(error);
-                    res.json({
-                        message: "There were an error with database connection",
-                        code: 500
+                    res.status(500).json({
+                        message: "There were an error with database connection"
                     });
                 }
                 res.status(200).json({
                     message: "Registered successfully",
-                    user: username,
-                    email: email,
-                    code: 200
+                    username: username
                 });
             })
         }
